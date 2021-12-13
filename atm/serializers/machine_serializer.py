eb7f649588ada522bb4853a4b036858dbfa0601d
denokenya <dennismuteti25@gmail.com>
2021-12-13 11:35:43 +0300

    create machine_serializer.py

---
+++ atm/serializers/machine_serializer.py
@@ -0,0 +1,19 @@
+from django.db.models import fields
+from rest_framework import serializers
+
+from models.machine_model import Machine
+
+
+class Machine_Serializer(serializers.ModelSerializer):
+   
+    class Meta:
+        model = Machine
+        fields = [
+            'machine_id',
+            'location',
+            'minimum_atm_balance',
+            'current_balance',
+            'last_refill_date',
+            'next_maintenance_date',  
+        ]
+        
