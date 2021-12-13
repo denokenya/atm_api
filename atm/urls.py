ff31380917e8a08b7c664febbfd959a218f603b7
denokenya <dennismuteti25@gmail.com>
2021-12-13 12:17:07 +0300

    register account url

--- atm/urls.py
+++ atm/urls.py
@@ -6,12 +6,16 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
 from atm.views.machine_view import Machine_ViewSet
+from atm.views.machine_refill_view import Machine_Refill_ViewSet
+from atm.views.account_view import Account_ViewSet
 
 
 
 router = DefaultRouter()
 
 router.register("machine",Machine_ViewSet, "machine")
+router.register("machine_refill",Machine_Refill_ViewSet, "machine_refill")
+router.register("account",Account_ViewSet, "account")
 
 
 
